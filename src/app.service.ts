88c8af48dabcfad249fcf967fb4db27282869145
bujosa <davidbujosa@gmail.com>
2021-06-04 02:48:06 -0400

    Install packages

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  uploadFile() {
+    const storage = new Storage();
+    return storage;
   }
 }
