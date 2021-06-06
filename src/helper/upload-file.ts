f79305baa1cf10d09ed3ded5063697607bbcef8c
bujosa <davidbujosa@gmail.com>
2021-06-06 03:12:10 -0400

    Install uuid

--- src/helper/upload-file.ts
+++ src/helper/upload-file.ts
@@ -1,12 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { storage } from 'firebase-admin';
+import { v4 as uuid } from 'uuid';
 
 export const uploadFileHelper = async (
   storage: storage.Storage,
   file: Express.Multer.File,
 ) => {
   const bucket = storage.bucket(process.env.BUCKET);
-  const fileName = `${Date.now()}.${file.originalname.split('.').pop()}`;
+  const fileName = `${Date.now()}-${uuid()}.${file.originalname
+    .split('.')
+    .pop()}`;
   const blob = bucket.file(fileName);
   const blobStream = blob.createWriteStream();
   const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
